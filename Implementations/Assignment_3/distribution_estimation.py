25d294f5343aa2d1490e09f52d997fabee3c1326
PrakharGoenka <prakhar.goinka@gmail.com>
2019-11-20 19:54:49 +0530

    Assignment 3: Bug Fixes.

--- Implementations/Assignment_3/distribution_estimation.py
+++ Implementations/Assignment_3/distribution_estimation.py
@@ -24,7 +24,7 @@ def gen_dataset(N, u_min, u_max):
 
     m = int(N * u)
 
-    # acertain that rounding off m, does not shift u in the prohibited range
+    # ascertain that rounding off m, does not shift u in the prohibited range
     if m == N * u_min:
         m -= 1
     if m == N * u_max:
@@ -41,7 +41,7 @@ def gen_dataset(N, u_min, u_max):
     return dataset, m
     
 
-def plot(y, filename):
+def plot(y, u, filename):
     '''
         function to plot y vs x
     '''
@@ -52,6 +52,9 @@ def plot(y, filename):
 
     plt.axis([0, 1, 0, 20])
     plt.plot(x, y)
+    plt.xlabel('µ')
+    plt.ylabel('posterior (β)')
+    plt.title('{} / Prior E[µ]: {}'.format(filename[0:-4], u))
     plt.savefig(filename)
     plt.close()
 
@@ -78,16 +81,19 @@ def sequential(dataset, N, m):
     result.append(temp)
 
     for n in dataset:
-        # update posterior to be the prior distribution for next iteration
-        a += n
-        b += 1 - n
         temp = []
 
         for i in range(101):
             u = i/100
-            temp.append(beta_distribution(a, b, u))
+            prior = beta_distribution(a, b, u)
+            posterior = ((u ** n) * ((1-u) ** (1-n))) * prior
+            temp.append(posterior)
 
         result.append(temp)
+
+        # update posterior to be the prior distribution for next iteration
+        a += n
+        b += 1 - n
 
     return result
 
@@ -116,24 +122,27 @@ def combined(dataset, N, m):
 
 
 def main():
-    dataset, m = gen_dataset(160, 0.4, 0.6)
+    N = 160
+    dataset, m = gen_dataset(N, 0.4, 0.6)
+    u = m/N
 
     # estimate distribution using sequential method
     print('For sequential\n')
-    result_s = sequential(dataset, 160, m)
+    result_s = sequential(dataset, N, m)
     print(result_s)
 
     # estimate distribution using combined method
     print('\n\nFor combined\n')
-    result_c = combined(dataset, 160, m)
+    result_c = combined(dataset, N, m)
     print(result_c)
 
 
     # plot results for sequential method
-    for i in range(0, 161, 20):
-        plot(result_s[i], 'sequential_' + str(i) + '.png')
+    for i in range(0, N+1, 20):
+        plot(result_s[i], u, 'sequential_{}.png'.format(i))
 
-    plot(result_c, 'combined.png')
+    # plot results for combined method
+    plot(result_c, u, 'combined.png')
 
 
 if __name__ == '__main__':
